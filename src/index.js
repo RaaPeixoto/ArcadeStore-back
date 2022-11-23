3f61fc3057ea08fd13ddc5e0b5efc83289a79162
Raísa Peixoto <raisacad.engcivil@gmail.com>
2022-11-23 14:57:09 -0300

    feat: add products routers, middlewares e controller

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import productsRouters from "./routers/productsRouters.js"
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-// colocar as rotas e importar
-//app.use(usersRouters);
+
+app.use(productsRouters);
 
 
 
